dbe830f881745a53f3df35cc44fb661ae6dda951
Ender2014 <lau.zihao@gmail.com>
2024-11-03 18:02:04 +0800

    minor edits

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
+// Global Variables
+const container = document.querySelector(".container");
+const header = document.querySelector(".header");
+const contentContainer = document.querySelector(".content");
+const addBtn = document.querySelector(".add-btn");
+const delBtn = document.querySelector(".del-btn");
+const remBtn = document.querySelector(".rem-btn");
+const status= document.querySelector(".status");
+
 const myLibrary = [];
 
+// Book Constructor
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -9,7 +19,32 @@ function Book(title, author, pages, read){
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read} read yet`;}
 }
 
-
 function addBookToLibrary(title, author, pages, read) {
-    // do stuff here
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+
+function displayBook(){
+    for(const book of myLibrary){
+        let card = document.createElement("div");
+        card.classList.add("cards");
+
+        let title = document.createElement("div");
+        title.classList.add("title");
+        title.textContent = `Title: ${book.title}`;
+        card.appendChild(title);
+
+        let pages = document.createElement("div");
+        pages.classList.add("pages");
+        pages.textContent = `Pages: ${book.pages}`;
+        card.appendChild(pages);
+
+        let author = document.createElement("div");
+        author.classList.add("author");
+        author.textContent = `Author: ${book.author}`;
+        card.appendChild(author);
+
+        contentContainer.appendChild(card);
+    }
 }

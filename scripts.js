c6cbce2bd2577b6f33df87a14d9223e081be1a28
Ender2014 <lau.zihao@gmail.com>
2024-11-03 15:11:47 +0800

    Initialize files

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+}
+
+Book.prototype.getInfo = function(){
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read} read yet`;
+}
